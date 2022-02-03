79cfdfa2f60b93dd9ac4f24b57b2888680dbf9ee
Yu Taninari <taninari_yu@cyberagnet.co.jp>
2022-02-03 17:29:20 +0900

    remove comment code

--- main.go
+++ main.go
@@ -8,10 +8,7 @@ import (
 func main() {
 	mh := dd.GetMuteHostList()
 	mm := dd.GetMuteMonitorList()
-	//for _, data := range mm {
-	//	fmt.Println(data.GetHostName())
-	//	fmt.Println(data.GetMInfo())
-	//}
+
 	slack.PostMessageHost(mh)
 	slack.PostMessageMonitor(mm)
 }
