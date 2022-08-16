71097497d571c7188108f43a3146b9c1f9b00ee2
Yu Taninari <taninari_yu@cyberagnet.co.jp>
2022-08-16 16:36:38 +0900

    add function which mute monitor and host

--- slack/slack_noti.go
+++ slack/slack_noti.go
@@ -1,45 +1,73 @@
 package slack
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/slack-go/slack"
 	"github.com/tani-yu/dd-noti/datadog"
 )
 
 func PostMessageMonitor(mm []datadog.MutedMonitor) {
+	var trigger = true
+	var err error
 	// アクセストークンを使用してクライアントを生成する
 	token := os.Getenv("SLACK_API_TOKEN")
 	channel := os.Getenv("SLACK_CHANNEL")
-	c := slack.New(token)
 
-	attachment := slack.Attachment{
-		Text:   "muteになっているMonitorをお知らせします",
-		Fields: createAttachmentFields(mm),
+	if os.Getenv("NOTIFY_MOMIT") != "" {
+		trigger, err = strconv.ParseBool(os.Getenv("NOTIFY_MOMIT"))
+		if err != nil {
+			trigger = true
+			fmt.Fprintf(os.Stderr, "Error Failed to cast NOTIFY_MOMIT: %v\n", err)
+		}
 	}
 
-	// MsgOptionText() の第二引数に true を設定すると特殊文字をエスケープする
-	_, _, err := c.PostMessage(channel, slack.MsgOptionText("datadog muted noti", true), slack.MsgOptionAttachments(attachment))
-	if err != nil {
-		panic(err)
+	if trigger {
+		c := slack.New(token)
+
+		attachment := slack.Attachment{
+			Text:   "muteになっているMonitorをお知らせします",
+			Fields: createAttachmentFields(mm),
+		}
+
+		// MsgOptionText() の第二引数に true を設定すると特殊文字をエスケープする
+		_, _, err := c.PostMessage(channel, slack.MsgOptionText("datadog muted noti", true), slack.MsgOptionAttachments(attachment))
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
 func PostMessageHost(mh []string) {
+	var trigger = true
+	var err error
 	// アクセストークンを使用してクライアントを生成する
 	token := os.Getenv("SLACK_API_TOKEN")
 	channel := os.Getenv("SLACK_CHANNEL")
-	c := slack.New(token)
 
-	attachment := slack.Attachment{
-		Text:   "muteになっているHostお知らせします",
-		Fields: createAttachmentFieldsFromString(mh),
+	if os.Getenv("NOTIFY_HOST") != "" {
+		trigger, err = strconv.ParseBool(os.Getenv("NOTIFY_HOST"))
+		if err != nil {
+			trigger = true
+			fmt.Fprintf(os.Stderr, "Error Failed to cast NOTIFY_HOST: %v\n", err)
+		}
 	}
 
-	// MsgOptionText() の第二引数に true を設定すると特殊文字をエスケープする
-	_, _, err := c.PostMessage(channel, slack.MsgOptionText("datadog muted noti", true), slack.MsgOptionAttachments(attachment))
-	if err != nil {
-		panic(err)
+	if trigger {
+		c := slack.New(token)
+
+		attachment := slack.Attachment{
+			Text:   "muteになっているHostお知らせします",
+			Fields: createAttachmentFieldsFromString(mh),
+		}
+
+		// MsgOptionText() の第二引数に true を設定すると特殊文字をエスケープする
+		_, _, err := c.PostMessage(channel, slack.MsgOptionText("datadog muted noti", true), slack.MsgOptionAttachments(attachment))
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
